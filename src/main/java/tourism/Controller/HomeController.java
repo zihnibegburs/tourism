3512a5443d723f53107d72d44bcade2e9eef7a1d
zihni <zihni.begburs@eyesclear.com>
2019-07-21 11:08:45 +0300

    first commit2

---
+++ src/main/java/tourism/Controller/HomeController.java
@@ -0,0 +1,19 @@
+package tourism.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+    public String Deneme(Model model){
+        String name = "Zihni";
+        model.addAttribute("name", name);
+        return "welcom";
+    }
+
+}
---
+++ src/main/java/tourism/SpringBegbursBootApplication.java
@@ -0,0 +1,12 @@
+package tourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBegbursBootApplication {
+    public static void main(String [] args){
+        SpringApplication.run(SpringBegbursBootApplication.class,args);
+    }
+}
